ffe3ae6d69540cc102fbc30367194b9be4a54813
payne-chris-r <06cpayne@gmail.com>
2015-08-24 11:07:10 -0400

    Created routes. Signup and Login are now possible. Have not tested them yet.

--- app.js
+++ app.js
@@ -11,7 +11,12 @@ app.use('/', require('./routes/index.js'));
 //   User = models.User;
 /*other models, like tag, profile, etc.*/
 
+process.env.SESSION_SECRET || require('dotenv').load();
 
+var passport = require('./lib/passport.js');
 
+app.use(passport.initialize());
+
+app.use(passport.session());
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,57 @@
 'use strict'
 
 var express = require('express');
-
 var router = express.Router();
+var passport = require('passport');
+var bodyParser = require('body-parser');
+var bcrypt = require('bcrypt');
+//async
 
 router.get('/', function(req,res){
   console.log("Hello world.");
   res.send("Welcome to Cara's app.");
 });
 
-// router.route('/')
+
+
+router.route('/login')
+  .get(function(req,res,next){
+    res.sendStatus(405);
+  })
+  .post(passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+  }));
+
+router.route('/signup')
+  .get(function(req,res,next){
+    res.sendStatus(405);
+  })
+  .post(function(req,res,next){
+    if(!req.body || !req.body.username || !req.body.password){
+      return next(err);
+    }
+    async.waterfall([
+      function(cb){
+        bcrypt.genSalt(16,cb);
+      },
+      function(salt,cb){
+        bcrypt.hash(req.body.username, salt, cb);
+      },
+      function(hash,cb){
+        User.create({
+          localName: req.body.username,
+          localPass: hash
+        }).then(function(user){
+          cb(null,user);
+        }).catch(cb);
+      }
+      ], function(err,res){
+        if(err){
+          return next(err);
+        }
+        res.sendStatus(201);
+      });
+  });
 
 module.exports = router;
